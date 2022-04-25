b9d60cda47a5d1228fffaf6ab3b2809e73db6a3c
Tyler Beetle <twbeetle@outlook.com>
2022-04-24 23:49:53 -0400

    adapter design pattern

---
+++ adapter_design_pattern/Tune.java
@@ -0,0 +1,10 @@
+package adapter_design_pattern;
+/**
+ * @author Tyler Beetle
+ * @description A class Tune that creates multiple gettorsß
+ */
+public interface Tune {
+    public String getArtistName();
+    public String getDisplayTitle();
+    public String getCategory();
+}
